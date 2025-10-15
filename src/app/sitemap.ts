f58bf0d8e4043c561dfc6b47b21e1b2842cacb55
haonguyenth <hao.nguyenthe@tribox.me>
2025-10-15 21:33:57 +0700

    enhance: sitexml file

---
+++ src/app/sitemap.ts
@@ -0,0 +1,72 @@
+// app/sitemap.ts
+import { articles, carRentalData } from "@/lib/data";
+import type { MetadataRoute } from "next"; // Sử dụng 'import type' cho an toàn kiểu
+
+// Hàm chuyển đổi ngày từ 'dd/mm/yyyy' sang 'YYYY-MM-DD'
+function formatDate(dateString: string): string {
+  const [day, month, year] = dateString.split("/");
+  return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+}
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteUrl = "https://www.dvdldaiduong.com";
+
+  const uniqueArticles = [
+    ...new Map(articles.map((item) => [item.slug, item])).values(),
+  ];
+
+  const articleUrls: MetadataRoute.Sitemap = uniqueArticles.map((article) => ({
+    url: `${siteUrl}/tin-tuc/${article.slug}`,
+    lastModified: formatDate(article.date),
+    changeFrequency: "monthly",
+    priority: 0.8,
+  }));
+
+  const uniqueCarRentalArticles = [
+    ...new Map(carRentalData.map((item) => [item.slug, item])).values(),
+  ];
+
+  const carRentalUrls: MetadataRoute.Sitemap = uniqueCarRentalArticles.map(
+    (article) => ({
+      url: `${siteUrl}/tin-tuc/${article.slug}`,
+      lastModified: Date.now() as unknown as string,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    })
+  );
+
+  const staticUrls: MetadataRoute.Sitemap = [
+    {
+      url: siteUrl,
+      lastModified: new Date().toISOString().split("T")[0],
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${siteUrl}/bang-gia`,
+      lastModified: new Date().toISOString().split("T")[0],
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${siteUrl}/gioi-thieu`,
+      lastModified: new Date().toISOString().split("T")[0],
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${siteUrl}/lien-he`,
+      lastModified: new Date().toISOString().split("T")[0],
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${siteUrl}/tin-tuc`,
+      lastModified: new Date().toISOString().split("T")[0],
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+  ];
+
+  return [...staticUrls, ...articleUrls, ...carRentalUrls];
+}
